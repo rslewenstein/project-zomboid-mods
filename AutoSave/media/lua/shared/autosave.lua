e7b89364601ab3aa0dc293a67771ff5a418102aa
rslewenstein <rafasoaresjf@gmail.com>
2026-02-24 21:19:22 -0300

    autosave

---
+++ AutoSave/media/lua/shared/autosave.lua
@@ -0,0 +1,51 @@
+local SAVE_INTERVAL = 60
+local lastSave = 0
+
+
+-- =========================
+-- LOG TXT (API NATIVA)
+-- =========================
+local function writeLog()
+
+    local path = getFileWriter("autosave_log.txt", true, false)
+
+    if path then
+        local time = os.date("%d/%m/%Y %H:%M:%S")
+        path:write("Save realizado em: "..time.."\n")
+        path:close()
+    end
+
+end
+
+
+
+-- =========================
+-- SAVE
+-- =========================
+local function doSave()
+
+    print("[AutoSave] Salvando jogo...")
+
+    GameWindow.save(true)
+
+    writeLog()
+
+    print("[AutoSave] Save concluído!")
+
+end
+
+
+
+-- =========================
+-- TIMER
+-- =========================
+Events.OnTick.Add(function()
+
+    local now = os.time()
+
+    if now - lastSave >= SAVE_INTERVAL then
+        lastSave = now
+        doSave()
+    end
+
+end)
