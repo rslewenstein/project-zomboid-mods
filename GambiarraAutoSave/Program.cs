84155783659f2b9bcc7ba021d592b5e5c33aba7c
rslewenstein <rafasoaresjf@gmail.com>
2026-02-28 23:15:19 -0500

    adicionando gambiarra para salvar

---
+++ GambiarraAutoSave/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+
+class GambiarraAutoSave
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NameUser());
+        var filename = GetFileName();
+    }
+
+    private static string NameUser()
+    {
+        return Environment.UserName;
+    }
+
+    private static string? GetFileName()
+    {
+        try
+        {
+            string path = @"C:\Users\RLewenstein\Zomboid\Logs";
+
+            if (!Directory.Exists(path))
+                return null;
+
+            return Directory.EnumerateFiles(path, "*DebugLog*.txt")
+                    .Select(Path.GetFileName)
+                    .FirstOrDefault();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+            return null;
+        }
+    }
+
+}
+
