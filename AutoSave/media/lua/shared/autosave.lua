30df928d7a59b5c9e43386a60e7029892b40e714
rslewenstein <rafasoaresjf@gmail.com>
2026-02-28 22:00:31 -0500

    changing log function

--- AutoSave/media/lua/shared/autosave.lua
+++ AutoSave/media/lua/shared/autosave.lua
@@ -1,40 +1,21 @@
 local SAVE_INTERVAL = 600
 local lastSave = 0
 
-
 -- =========================
 -- LOG TXT (API NATIVA)
 -- =========================
 local function writeLog()
-
-    local path = getFileWriter("autosave_log.txt", true, false)
-
-    if path then
-        local time = os.date("%d/%m/%Y %H:%M:%S")
-        path:write("Save realizado em: "..time.."\n")
-        path:close()
-    end
-
+    local time = os.date("%d/%m/%Y %H:%M:%S")
+    print("[AUTOSAVE] - Save realizado em: "..time.."\n")
 end
-
-
 
 -- =========================
 -- SAVE
 -- =========================
 local function doSave()
-
-    print("[AutoSave] Salvando jogo...")
-
     GameWindow.save(true)
-
     writeLog()
-
-    print("[AutoSave] Save concluído!")
-
 end
-
-
 
 -- =========================
 -- TIMER
