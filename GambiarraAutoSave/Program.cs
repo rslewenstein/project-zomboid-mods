e5cc0b922041ec55b9306a58e26560084e83d68d
rslewenstein <rafasoaresjf@gmail.com>
2026-02-28 23:28:37 -0500

    adding function to readfile

--- GambiarraAutoSave/Program.cs
+++ GambiarraAutoSave/Program.cs
@@ -5,8 +5,15 @@ class GambiarraAutoSave
 {
     static void Main(string[] args)
     {
+        string path = @"C:\Users\RLewenstein\Zomboid\Logs";
         Console.WriteLine(NameUser());
-        var filename = GetFileName();
+        var filename = GetFileName(path);
+
+        if (!string.IsNullOrEmpty(path) || !string.IsNullOrEmpty(filename))
+        {
+            ReadFile(path, filename);
+        }
+        
     }
 
     private static string NameUser()
@@ -14,12 +21,10 @@ class GambiarraAutoSave
         return Environment.UserName;
     }
 
-    private static string? GetFileName()
+    private static string? GetFileName(string path)
     {
         try
         {
-            string path = @"C:\Users\RLewenstein\Zomboid\Logs";
-
             if (!Directory.Exists(path))
                 return null;
 
@@ -32,6 +37,14 @@ class GambiarraAutoSave
             Console.WriteLine(ex);
             return null;
         }
+    }
+
+    private static void ReadFile(string path, string? filename)
+    {
+        File.ReadLines(Path.Combine(path, filename))
+            .Where(line => line.Contains("[AUTOSAVE]"))
+            .ToList()
+            .ForEach(Console.WriteLine);
     }
 
 }
